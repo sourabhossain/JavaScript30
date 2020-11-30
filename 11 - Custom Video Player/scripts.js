6c1e9005cf91e37da01dcf284938f0d0289a1395
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-11-30 09:00:58 +0600

    Query Selector

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,13 @@
+// Get out elements
+const player = player.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+//  Build out functions
+
+
+// Hook up the event listeners
