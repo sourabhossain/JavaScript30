6aab8e757469d7d4f391868b7c2474633755ce85
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-01 10:23:59 +0600

    rgbSplit

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -66,5 +66,16 @@ function redEffect(pixels) {
         pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
         pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
     }
+
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+
     return pixels;
 }
