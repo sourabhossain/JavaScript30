e1f15f55f9f159bdd758370b2e9b18fbcebd80de
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-12-01 10:17:54 +0600

    paintToCanvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -6,20 +6,42 @@ const snap = document.querySelector('.snap');
 
 function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      .then(localMediaStream => {
-        console.log(localMediaStream);
-  
-        //  DEPRECIATION : 
-        //       The following has been depreceated by major browsers as of Chrome and Firefox.
-        //       video.src = window.URL.createObjectURL(localMediaStream);
-        //       Please refer to these:
-        //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-        //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
-  
-        video.srcObject = localMediaStream;
-        video.play();
-      })
-      .catch(err => {
-        console.error(`OH NO!!!`, err);
-      });
-  }
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+
+            //  DEPRECIATION : 
+            //       The following has been depreceated by major browsers as of Chrome and Firefox.
+            //       video.src = window.URL.createObjectURL(localMediaStream);
+            //       Please refer to these:
+            //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+            //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // mess with them
+        // pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+
+        // pixels = greenScreen(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
